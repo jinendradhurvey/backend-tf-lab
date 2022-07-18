bf75badae459ab9cf1971f51676bdd5b43f0499a
jinendradhurvey <jinendra.dhurvey@cloudreach.com>
2022-07-18 19:07:21 +0530

    adding dynamodb for lock table

--- backend.tf
+++ backend.tf
@@ -2,5 +2,6 @@ terraform {
     backend "s3" {
         bucket = "talent-academy-jinendra-lab-tfstates"
         key = "talent-academy/backend/terraform.tfstates"
+        dynamodb_table = "terraform-lock"
     }
 }
